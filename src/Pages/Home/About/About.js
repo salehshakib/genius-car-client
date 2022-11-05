1f44290e24c3589abfc9677be375d416ca875b4c
Saleh Shakib <55012686+salehshakib@users.noreply.github.com>
2022-11-05 13:16:17 +0600

    first commit

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -8,7 +8,7 @@ const About = () => {
             <div className="hero-content flex-col lg:flex-row">
                 <div className='relative w-1/2'>
                     <img src={person} alt="" className="w-4/5 h-full rounded-lg shadow-2xl" />
-                    <img src={parts}  alt="" className="absolute right-5 top-1/2 w-3/5 border-8 rounded-lg shadow-2xl" />
+                    <img src={parts}  alt="" className="absolute right-5 top-1/2 w-3/5 border-8 shadow-2xl" />
                 </div>
                 <div className='w-1/2'>
                     <p className="text-2xl font-bold text-orange-600">About Us</p>
